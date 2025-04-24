3ef3a05d3510f46f7df5c3108b683794c6b610b2
Mahyar Samani <msamani@ucdavis.edu>
2025-04-24 16:54:33 -0700

    Fixing error in adding util/data/*.json

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email="msamani@ucdavis.edu",
     url="https://github.com/mahyarsamani/experiment",
     packages=find_packages(),
-    package_data={"experiment/util": ["data/*.json"]},
+    package_data={"experiment.util": ["data/*.json"]},
     entry_points={
         "console_scripts": ["helper = experiment.cli.helper:main_function"]
     },
