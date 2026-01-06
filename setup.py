14b2a7a4428e2ecefddb87cb66fc2139676b7a22
Mahyar Samani <msamani@ucdavis.edu>
2026-01-05 20:01:26 -0800

    Updating version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="experiment",
-    version="0.22.2",
+    version="0.26.1.0",
     license="MIT",
     description="Experimentation tooling for gem5",
     author="Mahyar Samani",
